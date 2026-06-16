e8476c8a5884b94a6d3e1fccf648749206ac03bf
Pranav Sai <pranavgudepu24@gmail.com>
2026-06-16 22:26:41 +0530

    Fix GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import TeamProvider from './context/TeamContext.jsx'
 import './index.css'
-import ErrorBoundary
-from "./components/ErrorBoundary";
+import ErrorBoundary from "./components/ErrorBoundary"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-
-  <ErrorBoundary>
-
-    <TeamProvider>
-
-      <App />
-
-    </TeamProvider>
-
-  </ErrorBoundary>
-
-</BrowserRouter>
+  <HashRouter>
+    <ErrorBoundary>
+      <TeamProvider>
+        <App />
+      </TeamProvider>
+    </ErrorBoundary>
+  </HashRouter>
 )
